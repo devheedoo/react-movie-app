eee5a8240f2efa6b97f8ff5932e16d619a64567b
devheedoo <devheedoo@gmail.com>
2019-10-29 11:59:52 +0900

    #2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Potato from './Potato';
 
+// "Component is a function which returns HTML."
 function App() {
-  return <div><h1>react-movie-app</h1>Hello, I'm Choco.</div>;
+  return (
+    <div>
+      <h1>react-movie-app</h1>
+      Hello, I'm Choco.
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// React only render 1 component
 ReactDOM.render(<App />, document.getElementById('potato'));
