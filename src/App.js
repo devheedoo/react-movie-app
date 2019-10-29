70044447183b235e37a37d25ac96d1079a56ca1f
devheedoo <devheedoo@gmail.com>
2019-10-29 12:12:12 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Potato from './Potato';
 
 // "Component is a function which returns HTML."
 function App() {
   return (
     <div>
       <h1>react-movie-app</h1>
-      Hello, I'm Choco.
-      <Potato />
+      <p>Hello, I'm Choco.</p>
+      <Food name="kimchi" />
+      <Food name="ramen" />
+      <Food name="samgiopsal" />
     </div>
   );
+}
+
+// function Food(props) {
+//   return <h3>I like {props.name}!</h3>;
+// }
+
+// Same as above
+function Food({ name }) {
+  return <h3>I like {name}!</h3>;
 }
 
 export default App;
