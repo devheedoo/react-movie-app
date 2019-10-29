8e635fa924c6e1d95650a51a2aad45f82fd2f7b1
devheedoo <devheedoo@gmail.com>
2019-10-29 12:42:03 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 // "Component is a function which returns HTML."
 function App() {
   return (
     <div>
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} />)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} rating={dish.rating} />)}
     </div>
   );
 }
 
-function Food({ name }) {
-  return <h3>I like {name}!</h3>;
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h3>I like {name}!</h3>
+      <p>rating: {rating} / 5.0</p>
+    </div>
+  );
 }
 
 const foodILike = [
-  {id: 1, name: 'kimchi'},
-  {id: 2, name: 'ramen'},
-  {id: 3, name: 'samgiopsal'}
+  {id: 1, name: 'kimchi', rating: 4.5},
+  {id: 2, name: 'ramen', rating: 5},
+  {id: 3, name: 'samgiopsal', rating: 3.5}
 ];
 
-// function renderFood(dish) {
-//   return <Food name={dish.name} />;
-// }
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 export default App;
